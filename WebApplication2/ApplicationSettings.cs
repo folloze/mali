3c5329a5c7e853377557c68c90e39926d7facd9d
Guy Adar <guya@folloze.com>
2016-06-08 12:30:44 +0300

    initial commit

---
+++ WebApplication2/ApplicationSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Collections;
+using System.Text;
+using System.Configuration;
+
+namespace WebApplication2
+{
+    public class ApplicationSettings : ConfigurationSection
+    {
+        [ConfigurationProperty("path", DefaultValue = "", IsRequired = false)]
+        public string path
+        {
+            get
+            {
+                return (string)this["path"];
+            }
+            set
+            {
+                this["path"] = value;
+            }
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ScanFilesController.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using bxMSCLIScanner;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace ScanFilesBuffer.Controllers
+{
+    
+    [RoutePrefix("api")]
+    public class ScanFilesController : ApiController
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        [Route("scans")]
+        [HttpPost]
+
+        public HttpResponseMessage ScanFromUrl()
+        {
+            string filepath = Request.RequestUri.Query.Replace("?filepath=", "");
+            string guid = Guid.NewGuid().ToString();
+            WebApplication2.ApplicationSettings settings = (WebApplication2.ApplicationSettings)System.Configuration.ConfigurationManager.GetSection("folloze/url");
+
+            ScanExecuter scan = new ScanExecuter();
+            string info_message = string.Format("Incoming file path: {0} ...", filepath);
+            log.Info(info_message);
+
+            Task task = new Task(() => scan.execute(filepath, guid, settings.path));
+            task.Start();
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(guid, System.Text.Encoding.UTF8, "text/plain");
+            return resp;
+        }
+
+    }
+}
